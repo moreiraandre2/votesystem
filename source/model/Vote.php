f4871645e7f8f96f5b73943bedc802bf9454319d
Andre Moreira <andreinformatica@terra.com.br>
2021-04-12 18:17:16 -0300

    First Version

--- source/model/Vote.php
+++ source/model/Vote.php
@@ -5,29 +5,30 @@ namespace Source\Model;
 use Source\Utils\Connect;
 use Source\Model\Elector;
 use PDO;
+use PDOException;
 
 Class Vote
 {
     public function voteDeposit($token, $election, $elector, $candidate)
     {
         $electorObject = new Elector();
-        $elector = $electorObject->checkElectorToken($token);
+        $token = $electorObject->checkElectorToken($token);
 
         $checkVoteInElection = $this->checkVoteInElection($election, $elector);
 
-        if($elector && $checkVoteInElection){
-            $connect = new Connect();
-            $conn = $connect->connect();
-            $stmt = $conn->prepare("INSERT INTO votes(elector_id, candidate_id, election_id) VALUES(:elector, :candidate, :election)");
-            $stmt->bindParam(':elector', $elector);
-            $stmt->bindParam(':election', $election);
-            $stmt->bindParam(':candidate', $candidate);
-
-            if($stmt->execute()){
+        if($token && $checkVoteInElection){
+            try{
+                $connect = new Connect();
+                $conn = $connect->connect();
+                $stmt = $conn->prepare("INSERT INTO votes(elector_id, candidate_id, election_id) VALUES(:elector, :candidate, :election)");
+                $stmt->bindParam(':elector', $elector);
+                $stmt->bindParam(':election', $election);
+                $stmt->bindParam(':candidate', $candidate);
+                $stmt->execute();
                 return true;
             }
-            else{
-                return false;
+            catch(PDOException $e){
+                var_dump($e->getMessage());
             }
         }
         else{
@@ -43,7 +44,10 @@ Class Vote
         $stmt = $conn->prepare("SELECT * FROM votes WHERE election_id = :election AND elector_id = :elector");
         $stmt->bindParam(':election', $election);
         $stmt->bindParam(':elector', $elector);
-        if(!$stmt->execute()){
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        if(empty($result)){
             return true;
         }
         else{
